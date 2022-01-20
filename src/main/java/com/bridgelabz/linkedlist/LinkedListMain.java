7d6561de15b133747a36feb29863aa8e68b577b6
Textspot <sachinkatarki1995@gmail.com>
2022-01-20 16:19:58 +0530

    Added:Programme to Delete the first Element in the Linked List

--- src/main/java/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -11,6 +11,7 @@ public class LinkedListMain {
 		System.out.println("Enter 1 to add the data at the start");
 		System.out.println("Enter 2 to add the data at the end");
 		System.out.println("Enter 3 to insert data at the Middle");
+		System.out.println("Enter 4 to delete first element");
 		switch (scanner.nextInt()) {
 		case 1:
 			Operations.addeElementAtStart();
@@ -20,6 +21,9 @@ public class LinkedListMain {
 			break;
 		case 3:
 			Operations.insertInBetween();
+			break;
+		case 4:
+			Operations.deleteFirstNode();
 			break;
 
 		}
